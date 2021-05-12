bdd30a78816bce4f2c3174c0737cd80d01a1c52e
Marcio Gama <marciogama2000@gmail.com>
2021-05-12 12:31:36 -0300

    Initial Adjusts and Add Navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Navbar = () => (
+    <nav className="main-nav">
+        <a href="link" className="nav-logo-link">
+            <h4 className="navbar-logo-text">Bootcamp DevSuperior</h4>
+        </a>
+    </nav>
+);
+
+export default Navbar;
