a996712b17d1dbadca4ea096041e2d5306699260
Marcio Gama <marciogama2000@gmail.com>
2021-05-19 15:23:49 -0300

    CSS adjusts

--- src/pages/Search/components/UserCard/index.tsx
+++ src/pages/Search/components/UserCard/index.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
 import { ReactComponent as UserImage } from '../../../../core/assets/images/product.svg';
+import SimpleButton from '../../../../core/components/Button';
 
 import './styles.css'
 
 const UserCard = () => (
-    <div className="user-card">
-        
-        <UserImage className="user-image"/>
-        <div className="user-information-one">
-            <div>Repositorios postados</div>
-            <div>Seguidores</div>
-            <div>Seguindo</div>
-        </div>
-        <div className="user-information-two">
-            <h3>informações</h3>
-            <div>empresa: @ZupIT</div>
-            <div>Website/Blog: https:thewashington.dev</div>
-            <div>Localidade: Uberlândia</div>
-            <div>Membro desde: 19/10/2013</div>
-        </div>
+    <div className="container-information">
 
+        <div className="colImage">
+            <UserImage className="user-image" />
+            <div className="button-see-profile">
+                <SimpleButton text="Ver perfil" />
+            </div>
+        </div>
+        <div className="colInfo user-card">
+
+            <div className="user-information-one">
+                <div className="user-posted">
+                    Repositorios públicos:
+                <span className="-span-user-information">
+                        62
+                </span>
+                </div>
+                <div className="user-followers">
+                    Seguidores:
+                <span className="span-user-information">
+                        127
+                </span>
+                </div>
+                <div className="user-following">
+                    Seguindo:
+                <span className="span-user-information">
+                        416
+                </span>
+                </div>
+            </div>
+            <div className="user-information-two">
+                <h3 className="information-two-title">Informações</h3>
+                <div className="information-two-company">empresa:
+                    <span className="span-user-information">@ZupIT</span>
+                </div>
+                <div className="information-two-website">Website/Blog:
+                    <span className="span-user-information">
+                        https:thewashington.dev
+                    </span>
+                </div>
+                <div className="information-two-location">Localidade:
+                    <span className="span-user-information">
+                        Uberlândia
+                    </span>
+                </div>
+                <div className="information-two-membersince">Membro desde:
+                    <span className="span-user-information">
+                        19/10/2013
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
